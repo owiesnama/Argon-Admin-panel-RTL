066875bc9222a8de563680ad4ea9a95bff190f81
Ahmed Altaif <ahmedaltaif79@gmail.com>
2018-10-29 17:53:38 +0200

    changing direction to rtl

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,7 @@
 let mix = require('laravel-mix');
+require('laravel-mix-purgecss');
 
 
 
-mix.sass('assets/scss/argon.scss', 'assets/css/argon-rtl.css');
+mix.sass('assets/scss/argon.scss', 'assets/css/argon-rtl.css')
+    .purgeCss({folders: ['examples','./'],extensions:['html']});
